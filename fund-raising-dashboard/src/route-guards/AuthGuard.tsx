5ae458d9427e6915dbc1bf748129010e71ee2e9b
Muhammad-Shah-zaib <mshahzab.bscs23seecs@seecs.edu.pk>
2024-04-07 00:15:42 +0500

    added login page and a auth router guard(pseudocode only)

--- fund-raising-dashboard/src/route-guards/AuthGuard.tsx
+++ fund-raising-dashboard/src/route-guards/AuthGuard.tsx
@@ -2,20 +2,17 @@ import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom"
 
 function AuthGuard() {
-    const AUTH = { token: null }
+    const AUTH = { token: undefined }
     const navigate = useNavigate();
 
     // if the token is not present in the local storage then redirect to the login page
-
-
     useEffect(() => {
         if (!AUTH.token) navigate('/login')
         else navigate('/dashboard')
-    }, [])
+    }, [AUTH.token, navigate])
+
     return (
-        <>
-            <Outlet />
-        </>
+        <Outlet />
     )
 }
 
